432a02e4923b99bb960320aa41c8a84bd01c430f
andrewmlee <andrex.lee@gmail.com>
2019-10-27 18:46:03 +0200

    add describemore() function

--- mungepy.py
+++ mungepy.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# Filling NaN Values
+
 def fillmedian(data):
     median = data.median()
     data.fillna(median, inplace = True)
@@ -11,4 +13,32 @@ def fillmode(data):
 def fillmean(data):
     mean = data.mean()
     data.fillna(mean, inplace = True)
+
+# Describing Data
+
+def describemore(data):
+    count = data.count()
+    mean = data.mean()
+    median = data.median()
+    mode = data.mode()
+    std = data.std()
+    minimum = data.min()
+    quarter = int(data.quantile([0.25]))
+    half = int(data.quantile([0.50]))
+    threequarters = int(data.quantile([0.75]))
+    
+    maximum = data.max()
+    
+    print('Count    ' + str(count))
+    print('Mean     ' + str(mean))
+    print('Median   ' + str(mean))
+    print('Mode     ' + str(mean))
+    print('Std      ' + str(std))
+    print('Min      ' + str(minimum))
+    print('25%      ' + str(quarter))
+    print('50%      ' + str(half))
+    print('75%      ' + str(threequarters))
+    print('Max      ' + str(maximum))
+
+
 
